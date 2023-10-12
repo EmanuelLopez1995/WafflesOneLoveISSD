1dda366ab3db22154b9daa158625b57ed8197349
Ema <emanicolopez@gmail.com>
2023-10-12 00:06:42 -0300

    primeras ideas navBar y otros cambios

---
+++ w1l/src/components/NavBar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="navBar">
+        <nav>
+            <router-link to="/">Inicio</router-link>
+            <router-link to="/stock">Stock</router-link>
+            <router-link to="/proveedores">Proveedores</router-link>
+            <router-link to="/compras">Compras</router-link>
+            <router-link to="/listados">Listados</router-link>
+            <router-link to="/empleados">Empleados</router-link>
+        </nav>
+        <div class="seccionUsuarioNav">
+            <img src="@/assets/img/imagenPerfil.png" alt="Usuario" class="imagenUsuario">
+            <!-- <div class="datosUsuario">
+                <span class="nombreUsuario">Super Mila</span>
+                <span class="rolUsuario">Super Admin</span> 
+            </div> -->
+        </div>
+    </div>
+</template>
+
+<script>
+import './NavBar.scss'
+
+export default {
+    
+}
+</script>
---
+++ w1l/src/views/HomeView/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+      <h1>WAFFLES ONE LOVE</h1>
+  </div>
+</template>
+
+<script>
+import './HomeView.scss'
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
