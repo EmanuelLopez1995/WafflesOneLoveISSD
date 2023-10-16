b5644c00c515034c1b2bd5d8a6ae787734d56fca
Ema <emanicolopez@gmail.com>
2023-10-16 14:26:19 -0300

    cambios de router, y agregando login view

--- w1l/src/store/index.js
+++ w1l/src/store/index.js
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    usuarioAutenticado: false
   },
   getters: {
   },
   mutations: {
+    setUsuarioAutenticado(state, val){
+      state.usuarioAutenticado = val;
+    }
   },
   actions: {
   },
