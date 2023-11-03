cf1161984bab54c2be6dd16f1750f56293d6b143
Ema <emanicolopez@gmail.com>
2023-11-02 23:46:20 -0300

    Agregando snackBar

---
+++ w1l/src/components/SnackBar/SnackBar.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="text-center">
+
+    <v-snackbar
+        :color="exitoso ? 'green-darken-4' : 'red-darken-4'"   
+        v-model="showSnackbar"
+        :timeout="timeout"
+    >
+    <v-icon v-if="exitoso"
+        start
+        icon="mdi-checkbox-marked-circle"
+    ></v-icon>
+    <v-icon v-else
+        start
+        icon="mdi-cancel"
+    ></v-icon>
+    <span class="mensajeDeSnackBar">
+        {{ exitoso ? 'OPERACIÓN EXITOSA' : 'OPERACIÓN FALLIDA' }}
+    </span>
+
+        <template v-slot:actions>
+            <v-btn
+            variant="text"
+            @click="showSnackbar = false"
+            >
+            Cerrar
+            </v-btn>
+        </template>
+    </v-snackbar>
+</div>
+</template>
+
+<script>
+
+import './SnackBar.scss'
+
+export default {
+
+    props: {
+        exitoso: {
+            type: Boolean,
+            required: true
+        }
+    },
+    data: () => ({
+        showSnackbar: null,
+        timeout: 1800,
+    }),
+    watch: {
+        showSnackbar(newValue) {
+            if (!newValue) {
+                this.$emit('timeout-completado');
+            }
+        }
+    },
+    created() {
+        this.showSnackbar = true;
+    },
+  }
+</script>
