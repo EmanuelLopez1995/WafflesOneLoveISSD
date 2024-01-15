f3b5825185f39b9bd8654f461bd3f5c37bace144
Nacho <santamariaign@live.com.ar>
2024-01-15 19:28:54 -0300

    Cambios para el eliminar empleados

--- w1l/src/components/listadosEmpleado/listadoEmpleados.vue
+++ w1l/src/components/listadosEmpleado/listadoEmpleados.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="listadoEmpleados">
-        <Table v-if="empleados" :contenido="empleados" :titulos="titulosTabla"/> 
+        <Table v-if="empleados" :contenido="empleados" :titulos="titulosTabla" @eliminarEmpleado="eliminarEmpleado"/> 
     </div>
 </template>
 
@@ -30,7 +30,27 @@ import Table from '../Table/Table.vue'
                .catch(error => {
                   console.error('Error al obtener empleados', error);
                 });
-           }
+           },
+           eliminarEmpleado(id) {
+              console.log(`Intentando eliminar al empleado con ID: ${id}`);
+
+              const url = `/employees?id=${id}`;
+
+              console.log(`Intentando eliminar al empleado con ID: ${id}`);
+
+              this.$http.delete(url)
+              
+                  .then(response => {
+                    console.log(`Intentando eliminar al empleado con ID: ${id}`);
+                      console.log('Respuesta de la API:', response.data);
+                      console.log('Empleado eliminado con éxito');
+                      console.log(`Eliminar empleado con ID: ${id}`);
+                      this.getDatosEmpleados();
+                  })
+                  .catch(error => {
+                      console.error('Error al eliminar empleado', error);
+                  });
+}
      },
   }
 </script>
