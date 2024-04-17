82b2a5a9496ae59bf88b0d30603b6c9dcfc3ff87
Ema <emanicolopez@gmail.com>
2024-04-16 23:32:08 -0300

    primeros pasos de editar de los listados

---
+++ w1l/src/components/editModal/EditModal.vue
@@ -0,0 +1,49 @@
+<template>
+    <VDialog
+        v-model="props.dialog"
+        width="auto"
+    >
+        <VCard
+            prepend-icon="ri-edit-2-fill"
+            title="Editar"
+            class="px-5"
+        >
+            <VForm @submit.prevent="confirmarEdicion" ref="form">
+                <slot></slot>
+                <VCol             
+                    cols="12"
+                    class="d-flex gap-4 justify-end mt-3"
+                >
+                    <VBtn
+                        color="secondary"
+                        variant="outlined"
+                        @click="closeDialog"
+                    >
+                        CERRAR
+                    </VBtn>
+                    <VBtn
+                        color="primary"
+                        variant="outlined"
+                        type="submit"
+                    >
+                        CONFIRMAR
+                    </VBtn>
+                </VCol>
+            </VForm>
+        </VCard>
+    </VDialog>
+</template>
+
+<script setup>
+
+const props = defineProps(['dialog']);
+const emit = defineEmits(['cerrarDialogo', 'confirmarDialogo']);
+
+const closeDialog = () => {
+  emit('cerrarDialogo');
+};
+
+const confirmarEdicion = () => {
+    emit('confirmarDialogo')
+}
+</script>
