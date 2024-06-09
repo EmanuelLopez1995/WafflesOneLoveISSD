c5ea6ba4e58f438e998d174bb4150fa9bbac99a5
Ema <emanicolopez@gmail.com>
2024-06-09 20:52:04 -0300

    usando store y cambio de api

---
+++ w1l/src/store/store.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia';
+
+export const useGeneralStore = defineStore('counter', {
+  state: () => ({
+    turnoEnCurso: false,
+  }),
+  actions: {
+  },
+  getters: {
+  }
+});
